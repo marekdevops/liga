6d6b434d55f746f6155308e6a492a0c60c007c0b
marek <marek104@gmail.com>
2025-06-11 12:47:33 +0200

    zzz

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [leagues, setLeagues] = useState([]);
 
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_URL}/leagues`)
+    fetch("/api/leagues")
       .then((res) => res.json())
       .then((data) => setLeagues(data))
       .catch((err) => console.error("Błąd pobierania danych:", err));
