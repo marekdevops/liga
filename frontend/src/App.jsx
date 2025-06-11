a71b0c7e1c61cfec6baf214fe0cf415477ea30e1
marek <marek104@gmail.com>
2025-06-11 15:29:28 +0200

    sss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [leagues, setLeagues] = useState([]);
 
   useEffect(() => {
-    fetch("/api/leagues/")
+    fetch("/leagues/")     
       .then((res) => res.json())
       .then((data) => setLeagues(data))
       .catch((err) => console.error("Błąd pobierania danych:", err));
