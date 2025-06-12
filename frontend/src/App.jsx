1b4450ec5c7057722ee5720355b058ea7658ae70
marek <marek104@gmail.com>
2025-06-12 20:10:30 +0200

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,23 @@ import LeagueForm from "./views/admin/LeagueForm";
 import TeamForm from "./views/admin/TeamForm";
 import PlayerForm from "./views/admin/PlayerForm";
 import LeagueDetails from "./views/public/LeagueDetails";
+import Home from "./views/Home";
+
 function App() {
   return (
     <Router>
-      <Routes>
-        {/* Widok publiczny */}
-        <Route path="/" element={<LeagueList />} />
+<Routes>
+  <Route path="/" element={<Home />} /> {/* 🏠 Strona startowa */}
 
-        {/* Widoki administracyjne */}
-        <Route path="/admin/leagues/new" element={<LeagueForm />} />
-        <Route path="/admin/teams/new" element={<TeamForm />} />
-        <Route path="/admin/players/new" element={<PlayerForm />} />
-        <Route path="/league/:leagueId" element={<LeagueDetails />} />
-      </Routes>
+  {/* Publiczne */}
+  <Route path="/leagues" element={<LeagueList />} />
+  <Route path="/league/:leagueId" element={<LeagueDetails />} />
+
+  {/* Admin */}
+  <Route path="/admin/leagues/new" element={<LeagueForm />} />
+  <Route path="/admin/teams/new" element={<TeamForm />} />
+  <Route path="/admin/players/new" element={<PlayerForm />} />
+</Routes>
     </Router>
   );
 }
---
+++ frontend/src/views/Home.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import "./Home.css"; // stylujemy osobno
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1>⚽ System Zarządzania Ligą Piłkarską</h1>
+      <div className="home-links">
+        <div>
+          <h2>👀 Widok publiczny</h2>
+          <Link to="/">Zobacz dostępne ligi</Link>
+        </div>
+        <div>
+          <h2>🛠️ Panel administracyjny</h2>
+          <ul>
+            <li><Link to="/admin/leagues/new">➕ Dodaj ligę</Link></li>
+            <li><Link to="/admin/teams/new">➕ Dodaj drużynę</Link></li>
+            <li><Link to="/admin/players/new">➕ Dodaj zawodnika</Link></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
