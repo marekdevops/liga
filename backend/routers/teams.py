0ac143ed71296941b2998bbe7e01d54e56ab09be
marek <marek104@gmail.com>
2025-06-13 12:54:50 +0200

    sss

--- backend/routers/teams.py
+++ backend/routers/teams.py
@@ -19,13 +19,13 @@ async def get_teams(session: AsyncSession = Depends(get_async_session)):
     result = await session.execute(select(Team))
     return result.scalars().all()
 
-@router.post("/")
-async def create_league(league: LeagueCreate, session: AsyncSession = Depends(get_async_session)):
-    new_league = League(**league.dict())
-    session.add(new_league)
-    await session.commit()
-    await session.refresh(new_league)
-    return new_league
+# @router.post("/")
+# async def create_league(league: LeagueCreate, session: AsyncSession = Depends(get_async_session)):
+#     new_league = LeaguBe(**league.dict())
+#     session.add(new_league)
+#     await session.commit()
+#     await session.refresh(new_league)
+#     return new_league
 
 @router.get("/{team_id}/players", response_model=list[PlayerRead])
 async def get_team_players(team_id: int, session: AsyncSession = Depends(get_async_session)):
