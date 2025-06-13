e5cac01487331499c73267cc55e23547d16de37c
marek <marek104@gmail.com>
2025-06-13 17:12:24 +0200

    ss

--- frontend/src/views/public/LeagueList.jsx
+++ frontend/src/views/public/LeagueList.jsx
@@ -27,11 +27,6 @@ export default function LeagueList() {
           </li>
         ))}
       </ul>
-      <li key={league.id}>
-  <Link to={`/league/${league.id}/teams`}>
-    {league.name}
-  </Link>
-</li>
     </div>
   );
 }
