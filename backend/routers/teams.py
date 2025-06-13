58044b9a06e04ad5624004e43c73722011d0ff13
marek <marek104@gmail.com>
2025-06-13 12:57:21 +0200

    szx

--- backend/routers/teams.py
+++ backend/routers/teams.py
@@ -6,36 +6,26 @@ from sqlalchemy.future import select
 
 from app.db import get_async_session
 from app.models.team import Team
-from app.models.league import League
 from app.models.player import Player
 from app.schemas.team import TeamCreate, TeamRead
-from app.schemas.league import LeagueCreate
 from app.schemas.player import PlayerRead
 
 router = APIRouter(prefix="/teams", tags=["teams"])
 
-@router.get("/")
+@router.get("/", response_model=list[TeamRead])
 async def get_teams(session: AsyncSession = Depends(get_async_session)):
     result = await session.execute(select(Team))
     return result.scalars().all()
 
-# @router.post("/")
-# async def create_league(league: LeagueCreate, session: AsyncSession = Depends(get_async_session)):
-#     new_league = LeaguBe(**league.dict())
-#     session.add(new_league)
-#     await session.commit()
-#     await session.refresh(new_league)
-#     return new_league
-
-@router.get("/{team_id}/players", response_model=list[PlayerRead])
-async def get_team_players(team_id: int, session: AsyncSession = Depends(get_async_session)):
-    result = await session.execute(select(Player).where(Player.team_id == team_id))
-    return result.scalars().all()
-
-@router.post("/teams/", response_model=TeamRead)
+@router.post("/", response_model=TeamRead)
 async def create_team(team: TeamCreate, session: AsyncSession = Depends(get_async_session)):
     db_team = Team(name=team.name, league_id=team.league_id)
     session.add(db_team)
     await session.commit()
     await session.refresh(db_team)
     return db_team
+
+@router.get("/{team_id}/players", response_model=list[PlayerRead])
+async def get_team_players(team_id: int, session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(Player).where(Player.team_id == team_id))
+    return result.scalars().all()
